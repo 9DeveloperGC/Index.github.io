b9a596b61ff52cfd088b12fedff7e0a6e64d46fe
9DeveloperGC <donasmainindex@gmail.com>
2026-09-01 19:02:40 -0600

    initial commit

--- index.js
+++ index.js
@@ -5,57 +5,57 @@ const productos = {
         {
             nombre: "Telera",
             precio: 2.50,
-            imagen: "/Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Concha",
             precio: 9.00,
-            imagen: "../Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Panadero",
             precio: 9.00,
-            imagen: "..Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Gusanito",
             precio: 9.00,
-            imagen: "...Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Bolita de queso",
             precio: 8.00,
-            imagen: ".../Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Rebanada",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Español",
             precio: 7.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Rol de canela",
             precio: 10.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Cuernito",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Empanada",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Barquillo",
             precio: 10.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         }
     ],
 
@@ -63,27 +63,27 @@ const productos = {
         {
             nombre: "Oreja",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Trenza",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Taco",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Rehilete",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Mil hojas",
             precio: 10.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         }
     ],
 
@@ -91,77 +91,77 @@ const productos = {
         {
             nombre: "Muffin",
             precio: 10.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Polvorón",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Bisquet",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Beso",
             precio: 10.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Galleta",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Multi",
             precio: 11.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Dona - Azucar/Chocolate",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Dona - Diseño",
             precio: 15.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Piedra",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Pastelitos",
             precio: 10.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Pay de queso",
             precio: 10.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Ojo",
             precio: 10.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Pan de elote - Rebanada",
             precio: 10.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Volteado de piña",
             precio: 15.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Pachucos",
             precio: 9.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         }
     ],
 
@@ -169,36 +169,36 @@ const productos = {
         {
             nombre: "Pan chino",
             precio: 8.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Pan de muerto",
             precio: 10.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Pan de muerto - Concha",
             precio: 12.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Rosca de reyes - pequeña",
             precio: 80.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Rosca de reyes - mediana",
             precio: 150.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         }, {
             nombre: "Rosca de reyes - grande",
             precio: 350.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         },
         {
             nombre: "Rosca de reyes - Feíte",
             precio: 500.00,
-            imagen: "Resources/Index_resources/mantenimiento.png"
+            imagen: "Resources/Index_resources/Mantenimiento.png"
         }
     ],
 
